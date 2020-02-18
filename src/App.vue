cb40a7a46f4090edd805fcba1ec5eff75704b18f
joaopaul167 <joaopaulo167@gmail.com>
2020-02-18 16:22:47 -0300

    Header pronto, ainda em duvida sobre o efeito dos botões

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderMenu/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderMenu from './components/HeaderMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderMenu
   }
 }
 </script>
 
 <style lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+    vertical-align: baseline;
+    list-style: none;
+    border: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    a {
+        text-decoration: none;
+    }
 }
 </style>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,69 @@
+<template>
+    <header>
+        <div class="div1">
+        <img src="https://techparty.faccat.br/images/logos/LogoTPtexto2.png" alt="Logo">
+        <ul >
+           <li><a href="">INICIO</a></li>
+            <li><a href="">PROGRAMAÇÃO</a></li>
+             <li><a href="">INSCRIÇÃO</a></li>
+             <li><a href="">SOBRE</a></li>
+             <li><a href="">PARCEIROS</a></li>
+            <li><a href="">LOCAL</a></li>    
+        </ul>
+    </div>
+    <div class="div2"></div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu'
+}
+</script>
+
+<style scoped lang="scss">
+    $primary-color:#0e0b16;
+    $secundary-color: #E7DFDD;
+    header {
+        display: flex;
+        flex-direction: column;
+        .div1 {
+            background-color: $primary-color;
+            padding: 0 1rem;
+            display: flex;
+            justify-content: space-between;
+            
+            img { height: 6rem; }
+            
+            ul {
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                margin-right: 2rem;
+            
+                li {
+                    padding: 1rem 1.5rem;
+                    border-left: 1px solid $secundary-color;
+                    color: $secundary-color;
+                    &:first-child { border: none; }
+                
+                    a {color: $secundary-color;}
+                    
+                    a:hover{
+                        padding: 2px;
+                        background-color: $secundary-color;
+                        border-radius: 3px;
+                        color: $primary-color;
+                        border: 3px  outset $secundary-color;
+                    }
+                }
+            }
+        }
+        .div2 {
+            background-color: transparent;
+            background-image: linear-gradient(to bottom, $primary-color 5%, transparent);
+            height: 50px;
+            display: flex;
+        }
+    }
+</style>
