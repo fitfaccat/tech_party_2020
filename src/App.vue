f6fd9d016ad482912ba2ead497b1aec7f9cf4288
igorfelcam <igorfelcam@hotmail.com>
2020-02-19 00:12:39 -0300

    created speakers component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
 
     <HeaderMenu />
     <Banner />
+    <Speakers />
     
   </div>
 </template>
 
 <script>
-import HeaderMenu from './components/HeaderMenu.vue'
-import Banner from './components/Banner.vue'
+import HeaderMenu from './components/HeaderMenu.vue';
+import Banner from './components/Banner.vue';
+import Speakers from './components/Speakers.vue';
 
 export default {
   name: 'App',
   components: {
     HeaderMenu,
-    Banner
+    Banner,
+    Speakers
   }
 }
 </script>
@@ -27,15 +30,19 @@ export default {
   vertical-align: baseline;
   list-style: none;
   border: 0;
+  box-sizing: border-box;
   font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
 }
+html, body { height: 100vh; }
+
 #app {
+  width: 100%;
+  
   a { text-decoration: none; }
 }
 
 header {
   position: fixed;
-  width: 100vw;
   z-index: 99;
 }
 </style>
