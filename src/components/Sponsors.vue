93bb463f978d252132f499b1fc4a1aca3fee9ac7
joaopaul167 <joaopaulo167@gmail.com>
2020-02-20 13:55:03 -0300

    Sponsors Component finished, only remain the font

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -3,16 +3,27 @@
 		<div id="becomeSponsor">
 			SEJA VOCÊ UM PATROCINADOR, ENTRE EM CONTATO: TECHPARTYFACCAT@GMAIL.COM
 		</div>
-			<section id="conteinerPlatinum">
+		<section id="conteinerPlatinum">
 				<h2>PLATINUM</h2>
 				<div id="conteinerArticlePlatinum">
 					<article>
 						<div id="imagePlatinum">
-							<img src="#" alt="">
+							<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
 						</div>
 						<div id="textPlatinum">
 							<h3>PADARIA DO SEU ZÈ</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>	
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>	
+						</div>
+					</article>
+				</div>
+				<div id="conteinerArticlePlatinum">
+					<article>
+						<div id="imagePlatinum">
+							<img src="https://coletivolirico.com.br/wp-content/uploads/2019/01/bach_home.jpg" alt="">
+						</div>
+						<div id="textPlatinum">
+							<h3>PADARIA DO SEU ZÈ</h3>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>	
 						</div>
 					</article>
 				</div>
@@ -23,25 +34,128 @@
 					<div id="conteinerArticleGold">
 						<article>
 							<div id="imageGold">
-								<img src="#" alt="">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
 							</div>
 							<div id="textGold">
 								<h3>PADARIA DO SEU ZÈ</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>	
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
 							</div>
 						</article>
 					</div>
 					<div id="conteinerArticleGold">
 						<article>
 							<div id="imageGold">
-								<img src="#" alt="">
+								<img src="https://coletivolirico.com.br/wp-content/uploads/2019/01/bach_home.jpg" alt="">
 							</div>
 							<div id="textGold">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
+							</div>
+						</article>
+					</div>
+			</div>
+		</section>
+		<section id="conteinerSilver">
+			<h2>SILVER</h2>
+				<div id="conteinerSilverArticles">
+					<div id="conteinerArticleSilver">
+						<article>
+							<div id="imageSilver">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
+							</div>
+							<div id="textSilver">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
+							</div>
+						</article>
+					</div>
+					<div id="conteinerArticleSilver">
+						<article>
+							<div id="imageSilver">
+								<img src="https://coletivolirico.com.br/wp-content/uploads/2019/01/bach_home.jpg" alt="">
+							</div>
+							<div id="textSilver">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>	
+							</div>
+						</article>
+					</div>
+			</div>
+		</section>
+		<section id="conteinerBronze">
+			<h2>BRONZE</h2>
+				<div id="conteinerBronzeArticles">
+					<div id="conteinerArticleBronze">
+						<article>
+							<div id="imageBronze">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
+							</div>
+							<div id="textBronze">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>	
+							</div>
+						</article>
+					</div>
+					<div id="conteinerArticleBronze">
+						<article>
+							<div id="imageBronze">
+								<img src="https://coletivolirico.com.br/wp-content/uploads/2019/01/bach_home.jpg" alt="">
+							</div>
+							<div id="textBronze">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>	
+							</div>
+						</article>
+					</div>
+					<div id="conteinerArticleBronze">
+						<article>
+							<div id="imageBronze">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
+							</div>
+							<div id="textBronze">
 								<h3>PADARIA DO SEU ZÈ</h3>
 								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>	
 							</div>
 						</article>
+				</div>
+			</div>
+		</section>
+		<section id="conteinerSupport">
+			<h2>SUPPORT</h2>
+				<div id="conteinerSupportArticles">
+					<div id="conteinerArticleSupport">
+						<article>
+							<div id="imageSupport">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
+							</div>
+							<div id="textSupport">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
+							</div>
+						</article>
 					</div>
+					<div id="conteinerArticleSupport">
+						<article>
+							<div id="imageSupport">
+								<img src="https://coletivolirico.com.br/wp-content/uploads/2019/01/bach_home.jpg" alt="">
+							</div>
+							<div id="textSupport">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
+							</div>
+						</article>
+					</div>
+					<div id="conteinerArticleSupport">
+						<article>
+							<div id="imageSupport">
+								<img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg" alt="">
+							</div>
+							<div id="textSupport">
+								<h3>PADARIA DO SEU ZÈ</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>	
+							</div>
+						</article>
+				</div>
 			</div>
 		</section>
 	</section>
@@ -56,7 +170,7 @@ export default {
 <style scoped lang="scss">
 	$primary-color:#0E0B16;
   $secundary-color: #E7DFDD;
-	$h3font-size: 2rem;
+
 	
 	section{
 		background-color: $primary-color;
@@ -70,43 +184,45 @@ export default {
 			color: $secundary-color;
 		}
 		
-		#conteinerPlatinum{	
-			
-			h2{
-				display: flex;
-				background-color: silver;
+		#conteinerPlatinum{			
+			h2{ 
+				width: 100%;
+				background-color: #acafac;
 				height: 2rem;
 				text-align: center;
 				justify-content: space-around;
 			}
-			#conteinerArticlePlatinum{
-				padding: 1rem;
-				background-color: $secundary-color;
+			#conteinerArticlePlatinum{	
 				display: flex;
-				margin: 1rem 1rem;
+				background-color: $secundary-color;
+				height: calc(100vh*0.45);
+				padding: 1rem;
+				margin: 1rem;
 				
 				article{
-					margin: 0rem 3rem;
 					display: grid;
 					grid-template-columns: 1fr 2fr;
 					
 					#imagePlatinum{
-						height: 100vh 100%;
-						background: yellow;
-						justify-content: space-around;
+							max-width: 450px;
+							max-height: 450px;
+						img{
+							max-width: 100%;
+							max-height: 100%;
+						}
 					}
-					
+		
 					#textPlatinum{
 						h3{
-							display: flex;
 							text-align: center;
 							justify-content: space-around;
-							font-size: $h3font-size;
+							font-size: 2rem;
+							margin-bottom: 1rem;
 						}
 						p{
 							align-content: center;
-							text-align:left;
-							margin-left: 10px;
+							margin-right: 10px;
+							font-size:1rem;
 						}
 					
 					}
@@ -114,51 +230,211 @@ export default {
 			}
 		
 		}
-	}
-	#conteinerGold{
-		h2{
-				display: flex;
-				background-color: gold;
+		
+		#conteinerGold{			
+			h2{ 
+				width: 100%;
+				background-color: Gold;
 				height: 2rem;
 				text-align: center;
 				justify-content: space-around;
 			}
-		#conteinerGoldArticles{
+			
+			#conteinerGoldArticles{
 			display: grid;
 			grid-template-columns: 1fr 1fr;
-			#conteinerArticleGold{
-					padding: 1rem;
-					background-color: $secundary-color;
+				
+				#conteinerArticleGold{	
 					display: flex;
-					margin: 1rem 1rem;
+					background-color: $secundary-color;
+					height: calc(100vh*0.35);
+					padding: 1rem;
+					margin: 1rem;
 				
 					article{
-						margin: 0;
 						display: grid;
 						grid-template-columns: 1fr 2fr;
 					
 						#imageGold{
-							height: 15rem;
-							width: 15rem;
-							background: yellow;
-							justify-content: space-around;
+								max-width: 400px;
+								max-height: 400px;
+							img{
+								max-width: 100%;
+								max-height: 100%;
+							}
 						}
-					
+		
 						#textGold{
 							h3{
-								display: flex;
 								text-align: center;
 								justify-content: space-around;
-								font-size: $h3font-size;
+								font-size: 1rem;
 							}
 							p{
 								align-content: center;
-								text-align:left;
 								margin-left: 10px;
+								font-size: .7rem;
+							}
+					
 						}
 					}
 				}
 			}
 		}
-	}
+		
+		#conteinerSilver{			
+			h2{ 
+				width: 100%;
+				background-color: silver;
+				height: 2rem;
+				text-align: center;
+				justify-content: space-around;
+			}
+			
+			#conteinerSilverArticles{
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+				
+				#conteinerArticleSilver{
+					display: flex;
+					background-color: $secundary-color;
+					height: calc(100vh*0.25);
+					padding: 1rem;
+					margin: 1rem;
+				
+					article{
+						display: grid;
+						grid-template-columns: 1fr 2fr;
+					
+						#imageSilver{
+								max-width: 350px;
+								max-height: 350px;
+							img{
+								max-width: 100%;
+								max-height: 100%;
+							}
+						}
+		
+						#textSilver{
+							h3{
+								text-align: center;
+								justify-content: space-around;
+								font-size: 1rem;
+							}
+							p{
+								align-content: center;
+								margin-left: 10px;
+								font-size: .7rem;
+							}
+					
+						}
+					}
+				}
+			}
+		}
+		
+		#conteinerBronze{			
+			h2{ 
+				width: 100%;
+				background-color: #b87333;
+				height: 2rem;
+				text-align: center;
+				justify-content: space-around;
+			}
+			
+			#conteinerBronzeArticles{
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+				
+				#conteinerArticleBronze{
+					display: flex;
+					background-color: $secundary-color;
+					height: calc(100vh*0.20);
+					padding: 1rem;
+					margin: 1rem;
+				
+					article{
+						display: grid;
+						grid-template-columns: 1fr 2fr;
+					
+						#imageBronze{
+								max-width: 300px;
+								max-height: 300px;
+							img{
+								max-width: 100%;
+								max-height: 100%;
+							}
+						}
+		
+						#textBronze{
+							h3{
+								text-align: center;
+								justify-content: space-around;
+								font-size: .8rem;
+							}
+							p{
+								align-content: center;
+								margin-left: 10px;
+								font-size: .5rem;
+							}
+					
+						}
+					}
+				}
+			}
+		}
+		
+		#conteinerSupport{			
+			h2{ 
+				width: 100%;
+				background-color: #e84c3d;
+				height: 2rem;
+				text-align: center;
+				justify-content: space-around;
+			}
+			
+			#conteinerSupportArticles{
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+				
+				#conteinerArticleSupport{
+					display: flex;
+					background-color: $secundary-color;
+					height: calc(100vh*0.20);
+					padding: 1rem;
+					margin: 1rem;
+				
+					article{
+						display: grid;
+						grid-template-columns: 1fr 2fr;
+					
+						#imageSupport{
+								max-width: 300px;
+								max-height: 300px;
+							img{
+								max-width: 100%;
+								max-height: 100%;
+							}
+						}
+		
+						#textSupport{
+							h3{
+								text-align: center;
+								justify-content: space-around;
+								font-size: .8rem;
+							}
+							p{
+								align-content: center;
+								margin-left: 10px;
+								font-size: .5rem;
+							}
+					
+						}
+					}
+				}
+			}
+		}
+		
+}
+	
 </style>
