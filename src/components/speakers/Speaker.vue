19515a43ee72cdebec844924c3d2c909490788ca
Igor Felipe de Camargo <igor.camargo@cwi.com.br>
2020-02-28 14:28:52 -0300

    adjustments speaker component

---
+++ src/components/speakers/Speaker.vue
@@ -0,0 +1,114 @@
+<template>
+	<section :id="id">
+
+		<article class="perfil-photo" v-if="evenMod === 0">
+			<img :src="image" :alt="name">
+		</article>
+
+		<article class="perfil-info">
+			<div class="perfil-wrap">
+				<h2>{{ name }}</h2>
+				<div class="social-links-content">
+					<a href="#" class="social-links-items">
+						<img src="../../assets/f_logo_RGB-Blue_58.png" alt="Facebook">
+					</a>
+					<a href="#" class="social-links-items">
+						<img src="../../assets/linkedin.png" alt="LinkedIn">
+					</a>
+					<a href="#" class="social-links-items">
+						<img src="../../assets/Twitter_Social_Icon_Circle_Color.png" alt="Twiiter">
+					</a>
+				</div>
+			</div>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque consequatur velit placeat dignissimos beatae quae modi alias doloremque architecto vel.
+			</p>
+		</article>
+
+		<article class="perfil-photo" v-if="evenMod !== 0">
+			<img :src="image" :alt="name">
+		</article>
+
+	</section>
+</template>
+
+<script>
+export default {
+  name: 'Speaker',
+	props: {
+		index:	Number,
+		name: 	String,
+		id:			String,
+		image:	String
+	},
+	computed: {
+		evenMod() {
+			return this.index % 2;
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+@import './../../assets/sass/variables.scss';
+
+section {
+		display: flex;
+
+		h2 {
+			padding-bottom: 0;
+			color: $primary-color;
+		}
+		article {
+			flex: 1;
+			height: 300px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		.perfil-photo {
+			background: $tertiary-color;
+
+			img {
+				height: 280px;
+				width: 280px;
+				border-radius: 50%;
+				display: block;
+			}
+		}
+		.perfil-info {
+			display: flex;
+			flex-direction: column;
+			background: $secundary-color;
+
+			p,
+			.perfil-wrap { flex: 1 }
+			.perfil-wrap {
+				width: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: space-around;
+			
+				.social-links-content {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					.social-links-items {
+						img {
+							padding: .5rem;
+							display: block;
+							width: 48px;
+							
+							&:hover { opacity: .8; }
+						}
+					}
+				}
+			}
+			p {
+				padding: 1rem 2rem;
+				text-align: justify;
+			}
+		}
+	}
+</style>
